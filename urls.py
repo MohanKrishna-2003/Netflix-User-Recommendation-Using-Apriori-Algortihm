3d805d9cd485e56d3d88e667989a310327aaf396
Mohan Krishna Battala <112927860+MohanKrishna-2003@users.noreply.github.com>
2024-06-28 19:04:54 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from NetflixUsingML import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.result, name='result'),
-]
-
